3985d7f1f7c902ebdb001a8a9944181799b094c6
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2025-12-28 20:57:19 +0800

    most likely fixed the destroy problem

--- media/lua/client/ParadiseZ_Hook.lua
+++ media/lua/client/ParadiseZ_Hook.lua
@@ -1,40 +1,61 @@
 ParadiseZ = ParadiseZ or {}
 
+
+function ParadiseZ.isAdm()
+    return string.lower(getPlayer():getAccessLevel()) == "admin"
+end
+
 Events.OnGameStart.Add(function()
+
     ParadiseZ.ISServerSandboxOptionsUIonButtonApply = ISServerSandboxOptionsUI.onButtonApply
     function ISServerSandboxOptionsUI:onButtonApply()
         self:settingsFromUI(self.options)
---[[ 
-        local strList = SandboxVars.ParadiseZ.BlockedList
-        ParadiseZ.parseZone(strList)
-        if isClient() then
-            sendClientCommand("ParadiseZ", "SyncBlockedZones", {strList = strList})
-        else 
-            ParadiseZ.parseZone(strList)
-
-        end
- ]]
-
         ParadiseZ.echo("SandboxOptions Updated")
         self.options:sendToServer()
         getSandboxOptions():toLua()
         self:destroy()
     end
 
+    ParadiseZ.ISUnbarricadeAction_isValid = ISUnbarricadeAction.isValid
+    function ISUnbarricadeAction:isValid()
+        if ParadiseZ.isSafeZone() then 
+            return false
+        end
+        return ParadiseZ.ISUnbarricadeAction_isValid(self)
+    end
+    
+
+    ParadiseZ.ISDestroyCursor_canDestroy = ISDestroyCursor.canDestroy
+    function ISDestroyCursor:canDestroy(obj)
+        if ParadiseZ.isAdm() then return ParadiseZ.ISDestroyCursor_canDestroy(self, obj) end         
+        if ParadiseZ.isSafeZone() then 
+            return false
+        end	
+        return ParadiseZ.ISDestroyCursor_canDestroy(self, obj)
+    end
+
+    ParadiseZ.ISMoveablesAction_isValid = ISMoveablesAction.isValid
+    function ISMoveablesAction:isValid()
+        if ParadiseZ.isAdm() or not self.moveProps or (self.mode and not (self.mode == "scrap" or self.mode == "pickup")) then
+            return ParadiseZ.ISMoveablesAction_isValid(self)
+        end
+        return ParadiseZ.ISMoveablesAction_isValid(self)
+    end
 end)
 
 function ParadiseZ.hookSafety()
-        function ISSafetyUI:onMouseUp(x, y)
-            ParadiseZ.doToggle()
+    function ISSafetyUI:onMouseUp(x, y)
+        ParadiseZ.doToggle()
+    end
+    Events.OnKeyPressed.Remove(ISSafetyUI.onKeyPressed);
+    function ISSafetyUI.onKeyPressed(key)
+        if key == getCore():getKey("Toggle Safety") then
+            ParadiseZ.doToggle()                
         end
-        Events.OnKeyPressed.Remove(ISSafetyUI.onKeyPressed);
-        function ISSafetyUI.onKeyPressed(key)
-            if key == getCore():getKey("Toggle Safety") then
-                ParadiseZ.doToggle()                
-            end
-        end
-        Events.OnKeyPressed.Add(ISSafetyUI.onKeyPressed);
-        ParadiseZ.CheckSafetyHook = true
+    end
+    Events.OnKeyPressed.Add(ISSafetyUI.onKeyPressed);
+    ParadiseZ.CheckSafetyHook = true
 end
 
 Events.OnInitGlobalModData.Add(ParadiseZ.hookSafety)
+
