a75b9ed77557638a753f255b26958df323f461d0
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2026-03-05 22:16:41 +0800

    added the range staff clothes and replaced button icons

---
+++ media/lua/shared/ParadiseZ_Distro.lua
@@ -0,0 +1,14 @@
+require "Items/Distributions"
+require "Items/ProceduralDistributions"
+require "SuburbsDistributions"
+
+local locations = {
+	["CrateCostume"] = {obj = "ParadiseZ.Shirt_TheRange", rate = 4},
+}
+
+Events.OnPostDistributionMerge.Add(function()
+	for k, v in pairs(locations) do
+		table.insert(ProceduralDistributions.list[k].items, tostring(v.obj))
+		table.insert(ProceduralDistributions.list[k].items, v.rate)
+	end
+end)
