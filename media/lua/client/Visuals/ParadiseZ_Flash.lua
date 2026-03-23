89100ebaf6bd9db2f7429f685323b98d2942c394
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2026-03-23 18:17:40 +0800

    repatched with safer var type checks

--- media/lua/client/Visuals/ParadiseZ_Flash.lua
+++ media/lua/client/Visuals/ParadiseZ_Flash.lua
@@ -2,9 +2,9 @@ ParadiseZ = ParadiseZ or {}
 
 function ParadiseZ.initFlashData(pl)
     local md = pl:getModData()
-    if md.FlashAlpha == nil then md.FlashAlpha = 0 end
-    if md.FlashDecayRate == nil then md.FlashDecayRate = 0 end
-    if md.LifeBarFlash == nil then md.LifeBarFlash = 0 end
+    if type(md.FlashAlpha) ~= "number" then md.FlashAlpha = 0 end
+    if type(md.FlashDecayRate) ~= "number" then md.FlashDecayRate = 0 end
+    if type(md.LifeBarFlash) ~= "number" then md.LifeBarFlash = 0 end
 end
 
 function ParadiseZ.doFlash(targPl)
@@ -13,7 +13,7 @@ function ParadiseZ.doFlash(targPl)
     ParadiseZ.initFlashData(pl)
     local md = pl:getModData()
     local decay = SandboxVars.ParadiseZ.ThunderFlashDecay or 0.4
-    if decay <= 0 then decay = 0.4 end
+    if type(decay) ~= "number" or decay <= 0 then decay = 0.4 end
     md.FlashAlpha = 1.0
     md.FlashDecayRate = 1.0 / (decay * 60)
 end
@@ -34,6 +34,7 @@ function ParadiseZ.drawFlash()
 
     if md.LifeBarFlash > 0 then
         local decay = (LifeBarUI and LifeBarUI.flashDecayRate) or 0
+        if type(decay) ~= "number" then decay = 0 end
         md.LifeBarFlash = math.max(0, md.LifeBarFlash - decay)
         getRenderer():renderRect(0, 0, sw, sh, 1.0, 0.0, 0.0, md.LifeBarFlash)
     end
