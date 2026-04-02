8b1d5754e2e5892db804188a24dc9e53a0e9c2f7
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2026-04-03 04:50:32 +0800

    moved the container icons and def to the paradisetiles mod instead

--- media/lua/shared/Definitions/ParadiseZ_ContDef.lua
+++ media/lua/shared/Definitions/ParadiseZ_ContDef.lua
@@ -1,8 +1,16 @@
+--[[ 
+
+--moved to ParadiseTiles mod
+--ParadiseTiles_Cont.lua
+
 ContainerButtonIcons = ContainerButtonIcons or {}
 local t = {}
 t.TheRangeLocker = getTexture("media/ui/Container_TheRangeLocker.png")
 t.TheRangeBox = getTexture("media/ui/Container_TheRangeBox.png")
+t.TheRangeContainer = getTexture("media/ui/Container_TheRangeContainer.png")
 
 ContainerButtonIcons.TheRangeLocker = t.TheRangeLocker
 ContainerButtonIcons.TheRangeBox = t.TheRangeBox
+ContainerButtonIcons.TheRangeContainer = t.TheRangeContainer
 
+ ]]
