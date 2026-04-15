39a5e1c7c297d49751b5babcc9f512720e916740
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2026-04-15 10:12:21 +0800

    isfire delay fix

--- media/lua/client/Zones/ParadiseZ_isNoFire.lua
+++ media/lua/client/Zones/ParadiseZ_isNoFire.lua
@@ -4,10 +4,10 @@ local ticks = 0
 function ParadiseZ.noFireHandler(pl)
     local interval = SandboxVars.ParadiseZ.NoFireZoneInterval or 50
     ticks = ticks + 1
-    if ticks % interval ~= 0 then return end    
-    if ParadiseZ.isNoFireZone(pl) then
-        ParadiseZ.StopFire(pl, true)
-    end
+    if ticks % interval == 0 then return end    
+    if not ParadiseZ.isNoFireZone(pl) then return end
+    ParadiseZ.StopFire(pl, true)
+    
 end
 Events.OnPlayerUpdate.Remove(ParadiseZ.noFireHandler)
 Events.OnPlayerUpdate.Add(ParadiseZ.noFireHandler)
